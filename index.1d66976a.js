5a270ddc580d66366dca9bbe85a55b4d47b9c43b
OkMoroz <k.moroz.mail@gmail.com>
2024-11-14 14:49:58 +0200

    make build

---
+++ index.1d66976a.js
@@ -0,0 +1,2 @@
+Array.from(document.querySelector("table").rows).forEach(function(e){var l=e.cells[1].cloneNode(!0),r=e.cells[e.cells.length-1];e.insertBefore(l,r)});
+//# sourceMappingURL=index.1d66976a.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const table = document.querySelector("table");
+const changeTable = Array.from(table.rows);
+changeTable.forEach((element)=>{
+    const secondColumnClone = element.cells[1].cloneNode(true);
+    const lastCell = element.cells[element.cells.length - 1];
+    element.insertBefore(secondColumnClone, lastCell);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
